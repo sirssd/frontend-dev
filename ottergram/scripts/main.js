91464f52be86a3a325e7b0f13aca48eb8d1dbdcb
Sergey Durov <sergssd@yandex.com>
2020-09-30 18:39:06 +0500

    init: add ottergram project

---
+++ ottergram/scripts/main.js
@@ -0,0 +1,81 @@
+const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+const DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
+const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+const HIDDEN_DETAIL_CLASS = 'hidden-detail';
+const TINY_EFFECT_CLASS = 'is-tiny';
+const ESC_KEY = 27;
+
+function setDetails(imageUrl, titleText) {
+    'use strict';
+    let detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    let detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+    detailImage.setAttribute('src', imageUrl);
+    detailTitle.textContent = titleText;
+}
+
+function imageFromThumb(thumbnail) {
+    'use strict';
+    return thumbnail.getAttribute('data-image-url');
+}
+
+function titleFromThumb(thumbnail) {
+    'use strict';
+    return thumbnail.getAttribute('data-image-title');
+}
+
+function setDetailsFromThumb(thumbnail) {
+    setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+}
+
+function addThumbClickHandler(thumb) {
+    'use strict';
+    thumb.addEventListener('click', function (event) {
+        event.preventDefault();
+        setDetailsFromThumb(thumb);
+        showDetails();
+    });
+}
+
+function getThumbnailsArray() {
+    'use strict';
+    let thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+    let thumbnailArray = [].slice.call(thumbnails);
+    return thumbnailArray;
+}
+
+function hideDetails() {
+    'use strict';
+    document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails() {
+    'use strict';
+    var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+    frame.classList.add(TINY_EFFECT_CLASS);
+
+    setTimeout(function () {
+        frame.classList.remove(TINY_EFFECT_CLASS);
+    }, 50);
+}
+
+function addKeyPressHandler() {
+    'use strict';
+    document.body.addEventListener('keyup', function (event) {
+        event.preventDefault();
+        console.log(event.keyCode);
+        if (event.keyCode === ESC_KEY) {
+            hideDetails();
+        }
+    });
+}
+
+function initializeEvents() {
+    'use strict';
+    let thumbnails = getThumbnailsArray();
+    thumbnails.forEach(addThumbClickHandler);
+    addKeyPressHandler();
+}
+
+initializeEvents();
