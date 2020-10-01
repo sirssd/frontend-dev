8a50d33ad995717c3156dbc81fa03c7fae405c1a
Sergey Durov <sergssd@yandex.com>
2020-10-01 14:46:31 +0500

    feat: add DataStore function

--- coffeerun/scripts/datastore.js
+++ coffeerun/scripts/datastore.js
@@ -1,4 +1,27 @@
 (function() {
     'use strict';
-    let app = window.App || {};
+    let App = window.App || {};
+    function DataStore() {
+        this.data = {};
+    }
+
+    DataStore.prototype.add = function(key, val) {
+        this.data[key] = val;
+    }
+
+    DataStore.prototype.get = function(key) {
+        return this.data[key];
+    }
+
+    DataStore.prototype.getAll = function() {
+        return this.data;
+    }
+
+    DataStore.prototype.remove = function(key) {
+        delete this.data[key];
+    }
+
+    App.DataStore = DataStore;
+    window.App = App;
 })()
+
